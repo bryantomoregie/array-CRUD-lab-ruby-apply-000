9963381bbb63c21e32d78ba5ad6e03629f5f29ad
Bryant Omoregie <unclebryant@gmail.com>
2020-01-28 02:47:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+ my_array = []
 end
 
 def create_an_array
-  
+  ["", "", "", ""]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array << "string"
 end
 
 def add_element_to_start_of_array(array, element)
